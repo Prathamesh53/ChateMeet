b757223719f524190b2d9c3f8aa25178ac1e3a12
Prathamesh53 <prathameshdshinde53@gmail.com>
2024-07-02 13:52:57 +0530

    first commit

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -6,6 +6,4 @@ const appName = 'Ai Assistant';
 //media query to store size of device screen
 late Size mq;
 
-//chat gpt api key (Updated it)
-// String apiKey = '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';
 String apiKey = '';
